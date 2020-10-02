cdc72efb99a522c4a4358dbf0bd2d02ca9a79ea6
gabrielsebag <gabriel.sebag@efrei.net>
2020-10-02 10:49:46 +0200

    Corrected all compilation issues

--- www/pages/index.js
+++ www/pages/index.js
@@ -33,33 +33,35 @@ const Card = ({ children }) => (
 
 export default function Home() {
   return (
-    <div className="h-screen bg-gray-200 py-5">
+    <div className="h-screen bg-gray-200 pb-5 pt-6">
       <div className="wrap">
+        <Title className="text-center mb-5" heading={1}>McQueen Playground</Title>
         <Card>
-        {
-          [
-            ChevronUpIcon,
-            ChevronRightIcon,
-            ChevronDownIcon,
-            ChevronLeftIcon,
-            ArrowUpIcon,
-            ArrowRightIcon,
-            ArrowDownIcon,
-            ArrowLeftIcon,
-            CheckIcon,
-            WarningOutlineIcon,
-            WarningFillIcon,
-            InfoOutlineIcon,
-            InfoFillIcon,
-            SearchIcon,
-            CalendarIcon,
-            CoinIcon,
-            MapMarkerIcon,
-            StarIcon,
-            PhoneIcon,
-            LinkIcon
-          ].map((icon, i) => <span key={i}>{ icon({ size: 40 }) }</span>)
-        }
+          <Title heading={2}>Icons</Title>
+          {
+            [
+              ChevronUpIcon,
+              ChevronRightIcon,
+              ChevronDownIcon,
+              ChevronLeftIcon,
+              ArrowUpIcon,
+              ArrowRightIcon,
+              ArrowDownIcon,
+              ArrowLeftIcon,
+              CheckIcon,
+              WarningOutlineIcon,
+              WarningFillIcon,
+              InfoOutlineIcon,
+              InfoFillIcon,
+              SearchIcon,
+              CalendarIcon,
+              CoinIcon,
+              MapMarkerIcon,
+              StarIcon,
+              PhoneIcon,
+              LinkIcon
+            ].map((icon, i) => <span key={i}>{ icon({ size: 40 }) }</span>)
+          }
         </Card>
       </div>
     </div>
