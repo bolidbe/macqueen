ada8318905e9f8ee711f0fca0cb19b146f4dc14a
gabrielsebag <gabriel.sebag@efrei.net>
2020-10-05 14:23:08 +0200

    Added LoaderDots in export

--- packages/mcqueen-react/index.tsx
+++ packages/mcqueen-react/index.tsx
@@ -3,6 +3,7 @@ export { default as Text } from './components/Text';
 export { default as Button } from './components/Button';
 export { default as TextInput } from "./components/TextInput";
 export { default as TextArea } from "./components/TextArea";
+export { default as LoaderDots } from "./components/LoaderDots";
 export { default as Select } from "./components/Select";
 export { default as Checkbox } from "./components/Checkbox";
 export { default as Radio } from "./components/Radio";
