71f61e6164e20de2b2767bb355aca107d0e2fa41
Gabriel Sebag <gabriel.sebag@efrei.net>
2020-12-03 14:43:45 +0100

    npm link is working completely

--- www/next.config.js
+++ www/next.config.js
@@ -1,12 +1,13 @@
 // next.config.js
 const plugins = require('next-compose-plugins');
+
+/*
 const transpileModules = require('next-transpile-modules')([
-  '@bolid/mcqueen-react',
+  //'@bolid/mcqueen-react',
   '@bolid/mcqueen-icons'
 ])
+*/
 
 const nextConfig = {}
 
-module.exports = plugins([
-  transpileModules
-], nextConfig);
+module.exports = plugins([], nextConfig);
